dc67e4dd4527b7620901963f322c6804cc9c089d
zainiirochman <rochman279@gmail.com>
2025-10-20 13:54:52 +0700

    modified:   app/Http/Controllers/Admin/HistoryController.php 	new file:   public/image/books/1760943193_Gemini_Generated_Image_qdmc2rqdmc2rqdmc.png

--- app/Http/Controllers/Admin/HistoryController.php
+++ app/Http/Controllers/Admin/HistoryController.php
@@ -12,18 +12,37 @@ class HistoryController extends Controller
     public function index(Request $request)
     {
         $search = $request->input('search');
+        $month = $request->input('month');
+        $year = $request->input('year');
         
-        $histories = History::with(['buku', 'anggota'])
-            ->when($search, function($query) use ($search) {
-                $query->whereHas('buku', function($q) use ($search) {
-                    $q->where('title', 'like', "%{$search}%");
+        $query = History::with(['buku', 'anggota']);
+        
+        if ($search) {
+            $query->where(function($q) use ($search) {
+                $q->whereHas('buku', function($subQ) use ($search) {
+                    $subQ->where('title', 'like', "%{$search}%");
                 })
-                ->orWhereHas('anggota', function($q) use ($search) {
-                    $q->where('name', 'like', "%{$search}%");
+                ->orWhereHas('anggota', function($subQ) use ($search) {
+                    $subQ->where('name', 'like', "%{$search}%");
                 });
-            })
-            ->orderBy('tanggal_kembali', 'desc')
-            ->get();
+            });
+        }
+        
+        if ($month) {
+            $query->whereMonth('tanggal_kembali', (int)$month);
+        }
+        
+        if ($year) {
+            $query->whereYear('tanggal_kembali', (int)$year);
+        }
+        
+        $histories = $query->orderBy('tanggal_kembali', 'desc')->get();
+        
+        // Debug: tampilkan semua tanggal kembali yang ada
+        if ($histories->isEmpty() && ($month || $year)) {
+            $allHistories = History::select('tanggal_kembali')->get();
+            \Log::info('Available dates:', $allHistories->pluck('tanggal_kembali')->toArray());
+        }
 
         return view('admin.history.index', compact('histories'));
     }
