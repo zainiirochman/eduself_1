1878e1d0a996167354a17aa1fa654cb431d4c771
zainiirochman <rochman279@gmail.com>
2025-11-17 13:23:17 +0700

    modified:   api/index.php 	modified:   test 	modified:   vercel.json

--- api/index.php
+++ api/index.php
@@ -1,2 +1,9 @@
-<?php 
-require __DIR__. '/../public/index.php';
+<?php
+$public = realpath(__DIR__ . '/../public/index.php');
+if ($public && file_exists($public)) {
+    require $public;
+    return;
+}
+http_response_code(500);
+echo "Server misconfigured: public/index.php not found. Expected at: " . (__DIR__ . '/../public/index.php');
+exit(1);
